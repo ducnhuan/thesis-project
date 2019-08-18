be6b3202cfa5ad9a05cadd869b2159298fa3f4a2
ducnhuan <ducnhuan246@gmail.com>
2019-08-19 03:43:39 +0700

    complete ethereum, ready for test

--- config/ethereum.js
+++ config/ethereum.js
@@ -1,16 +1,16 @@
+ module.exports = 
+ {
+     account1:'0xE0cb3318D18be98b9ac0f24a64BCAC4E7c22BA4c',
+     privateKey1:'0xf6de6f6e43579cdfd8fbc4eced777345152ef60759b5a47a60fed00781422129',
+     percent:10,
+     httpProvider:'https://ropsten.infura.io/v3/4e589b7e57a74296bb83df4497c70002',
+     webSocketProvider:'wss://ropsten.infura.io/ws/v3/4e589b7e57a74296bb83df4497c70002'
+}
 // module.exports = 
 // {
-//     account1:'0xE0cb3318D18be98b9ac0f24a64BCAC4E7c22BA4c',
-//     privateKey1:'0xf6de6f6e43579cdfd8fbc4eced777345152ef60759b5a47a60fed00781422129',
+//     account1:'0x6c51e88c7D33B16cDd72849D590d657f18db091B',
+//     privateKey1:'0x2fc3d36aa741d2375cae7166ea356d18dbfb8579ea07ce7ede8cfe3adda96964',
 //     percent:10,
-//     httpProvider:'https://ropsten.infura.io/v3/4e589b7e57a74296bb83df4497c70002',
-//     webSocketProvider:'wss://ropsten.infura.io/ws/v3/4e589b7e57a74296bb83df4497c70002'
+//     httpProvider:'http://127.0.0.1:7545',
+//     webSocketProvider:'ws://127.0.0.1:7545'
 // }
-module.exports = 
-{
-    account1:'0x6c51e88c7D33B16cDd72849D590d657f18db091B',
-    privateKey1:'0x2fc3d36aa741d2375cae7166ea356d18dbfb8579ea07ce7ede8cfe3adda96964',
-    percent:10,
-    httpProvider:'http://127.0.0.1:7545',
-    webSocketProvider:'ws://127.0.0.1:7545'
-}
