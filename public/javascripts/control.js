023504990ee5ea1ca15d7a43f12f9dbb16785a3e
ducnhuan <ducnhuan246@gmail.com>
2019-09-14 03:25:37 +0700

    edit wss host

--- public/javascripts/control.js
+++ public/javascripts/control.js
@@ -7,7 +7,7 @@ function test()
     console.log('Work');
     var HOST =location.origin;
     console.log(HOST);
-    var ws = new WebSocket('ws://localhost:8080');
+    var ws = new WebSocket('wss://salesforce-payment.herokuapp.com:8080');
     ws.onmessage = function(event)
     {
         console.log(event.data);
