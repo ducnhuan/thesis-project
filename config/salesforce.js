bab2fa3dfbc4f8639b67bb55e56c51995b34395f
ducnhuan <ducnhuan246@gmail.com>
2019-10-10 09:24:14 +0700

    fix authentication error

--- config/salesforce.js
+++ config/salesforce.js
@@ -1,7 +1,7 @@
 module.exports = 
 {
     userName:'admin@vgu.com',
-    password:'123456aA@IHcn32cTsZ2E7yAttzD3slLZg',
+    password:'Nh321562630@C9vXYhY9e82pyyE1cFjBk8LQ',
     loginUrl:'https://ap15.salesforce.com',
     accessToken:'00D2v000001dnWt!ARQAQN723nqZ2W5UrRC14m7OOPNmGhKNMEi.1NreVVT8DTLKeThrNG_ihZj3xibhagTcWHoHAdietL.PjVV1Vl2vrQswAj7r',
     secretkey:'thesis2019'
